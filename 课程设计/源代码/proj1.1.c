92993ce670f4e238aaac7af543b1ef2b761367b6
lumos <630212894@qq.com>
2018-03-12 22:52:13 +0800

    hh

--- 课程设计/源代码/proj1.1.c
+++ 课程设计/源代码/proj1.1.c
@@ -1,28 +1,31 @@
 //编写一个C程序，用fread、fwrite等库函数实现文件拷贝功能
 #include <stdio.h>
+
 #define N 1024
-int main(int argc, char const *argv[])
+
+int main(void)
 {
+	int n = 0;
 	char buffer[N]={0};
-	int n;
 	FILE *fpRead;
 	FILE *fpWrite;
-	if((fpRead=fopen("FileRead.txt","r"))==NULL)
+	if((fpRead=fopen("/home/lumos/FileRead.txt","r"))==NULL)
 	{
 		printf("Fail!Can't open file\n");
 		return 0;
 	}
-	if((fpWrite=fopen("FileWrite.txt","w"))==NULL)
+	if((fpWrite=fopen("/home/lumos/FileWrite.txt","w"))==NULL)
 	{
 		printf("Fail!Can't open file\n");
 		fclose(fpRead);
 		return 0;
 	}
-	while((n=fread(buffer,1,N,fpRead)))
+	while((n=fread(buffer,sizeof(char),N,fpRead)))
 	{
-		fwrite(buffer,1,n,fpRead);
+		fwrite(buffer,sizeof(char),n,fpWrite);
 	}
 	fclose(fpRead);
 	fclose(fpWrite);
+	printf("Success!\n");
 	return 0;
 }
